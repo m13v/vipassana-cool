a366bc6c16791559dedd109946fdc5c7af896994
m13v <i@m13v.com>
2026-04-29 14:31:22 -0700

    Remove bg-white class from accountability partner means page

--- src/app/t/accountability-partner-means/page.tsx
+++ src/app/t/accountability-partner-means/page.tsx
@@ -223,7 +223,7 @@ export default function Page() {
   const faqJsonLd = faqPageSchema(faqs);
 
   return (
-    <article className="bg-white text-zinc-900">
+    <article className="text-zinc-900">
       <script
         type="application/ld+json"
         dangerouslySetInnerHTML={{
